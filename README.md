6d045c7f6241fefa3313b9087e892e40aa14769d
Nick G <gonzanick@gmail.com>
2022-04-01 16:45:15 -0600

    Created menu , scanner, and started while loop. Started README

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # JetsProject
+
+## Week 3 Homework
+
+# Overview
+
+Jets
+* Create a fleet of jets and/or other aircraft with different specializations
+* Allow the application to let the user select from a menu of different options to run
+* In order to pass, program MUST be able to complete all tasks in grading portion of the assignment
+* Make sure to follow the structure laid out overview to stay on track
+
+
+# Technologies/Skills
+
+* Java
+* Eclipse
+* Github
+* ArrayList
+* While loop
+* Methods
+* Classes and Constructors
+
+
+# Lessons Learned
+
+* 
--- src/com/skilldistillery/jets/app/JetsApplication.java
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -1,13 +1,37 @@
 package com.skilldistillery.jets.app;
 
+import java.util.Scanner;
+
 public class JetsApplication {
 
 	public static void main(String[] args) {
+		JetsApplication jetApp = new JetsApplication();
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("Select what you would like to do");
+			jetApp.menu();
+			int userInput = sc.nextInt();
+			
+			if (userInput == 1) {
+				
+			}
+			
+		}
 		
 
 	}
 	
 	public void menu() {
+		System.out.println("1. List Fleet");
+		System.out.println("2. Fly all jets");
+		System.out.println("3. View fastest jet");
+		System.out.println("4. View jet with longest range");
+		System.out.println("5. Load all Cargo Jets");
+		System.out.println("6. Dogfight!");
+		System.out.println("7. Add a jet to the Fleet");
+		System.out.println("8. Remove a jet from the Fleet");
+		System.out.println("9. Quit");
 		
 		
 	}

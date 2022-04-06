3342d498042572c20afeaff963f5a9c077840bdd
Nick G <gonzanick@gmail.com>
2022-04-05 21:13:18 -0600

    PROJECT INCOMPLETE

--- src/com/skilldistillery/jets/app/JetsApplication.java
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -3,6 +3,8 @@ package com.skilldistillery.jets.app;
 import java.util.Scanner;
 
 import com.skilldistillery.jets.entities.AirField;
+import com.skilldistillery.jets.entities.Fighter;
+import com.skilldistillery.jets.entities.MilitaryCargo;
 
 public class JetsApplication {
 
@@ -10,24 +12,61 @@ public class JetsApplication {
 		JetsApplication jetApp = new JetsApplication();
 		Scanner sc = new Scanner(System.in);
 		AirField airFD = new AirField();
-		
-		while(true) {
+
+		while (true) {
 			System.out.println("Select what you would like to do");
 			jetApp.menu();
 			int userInput = sc.nextInt();
-			
+
+			if (userInput == 9) {
+				break;
+			}
+
 			if (userInput == 1) {
 				airFD.readFleet();
 				airFD.listFleet();
 				System.out.println();
+
+			}
+			if (userInput == 2) {
+
+			}
+			if (userInput == 3) {
+
+			}
+			if (userInput == 4) {
 				
 			}
-			
+			if (userInput == 5) {
+				MilitaryCargo mc = new MilitaryCargo();
+				mc.loadCargo();
+				System.out.println();
+				
+			}
+			if (userInput == 6) {
+				Fighter fight = new Fighter();
+				fight.fight();
+				System.out.println();
+			}
+			if (userInput == 7) {
+				System.out.println("Enter the type of jet: (Fighter, Passenger, or MilitaryCargo)\n"
+						+ "Then enter the model, speed, range, and price of the jet.");
+				String userInputType = sc.next();
+				String userInputModel = sc.next();
+				double userInputSpeed = sc.nextDouble();
+				int userInputRange = sc.nextInt();
+				long userInputPrice = sc.nextLong();
+				//jetList.add(userInputType, userInputModel, userInputSpeed, userInputRange, userInputRange, userInputPrice);
+				
+			}
+			if (userInput == 8) {
+				
+			}
+
 		}
-		
 
 	}
-	
+
 	public void menu() {
 		System.out.println("1. List Fleet");
 		System.out.println("2. Fly all jets");
@@ -38,16 +77,12 @@ public class JetsApplication {
 		System.out.println("7. Add a jet to the Fleet");
 		System.out.println("8. Remove a jet from the Fleet");
 		System.out.println("9. Quit");
-		
-		
+
 	}
-	
+
 	public void flyAll() {
-		
+
 	}
-	
-//	public void listFleet() {
-//		
-//	}
+
 
 }
--- src/com/skilldistillery/jets/entities/AirField.java
+++ src/com/skilldistillery/jets/entities/AirField.java
@@ -8,8 +8,7 @@ import java.util.List;
 
 public class AirField {
 	private List<Jet> jets;
-	
-	
+
 	public List<Jet> getJets() {
 		return jets;
 	}
@@ -18,51 +17,55 @@ public class AirField {
 		this.jets = jets;
 	}
 
-	public AirField() { }
-	
-	public void readFleet() {
-		
-		try ( BufferedReader bufIn = new BufferedReader(new FileReader("jets.txt")) ) {
-			  String line;
-			  List<Jet> jetList = new ArrayList<>();
-			  while ((line = bufIn.readLine()) != null) {
-			    String[] row = line.split(",");
-			    String type = row[0];
-			    String model = row[1];
-			    Double speed = Double.parseDouble(row[2]);
-			    Integer range = Integer.parseInt(row[3]);
-			    Long price = Long.parseLong(row[4]);
-			    if (type.equals("MilitaryCargo")) {
-			    	Jet temp = new MilitaryCargo(type,model, speed, range, price);
-			    	jetList.add(temp);
-			    	
-			    }
-			    else if (type.equals("Fighter")) {
-			    	jetList.add(new Fighter(type,model, speed, range, price));
-			    }
-			    else if (type.equals("Passenger")) {
-			    	jetList.add(new Passenger(type,model, speed, range, price));
-			    }
-			    
-			    
-			  }
-			  jets = jetList;
-			  
-			}
-			catch (IOException e) {
-			  System.err.println(e);
-			}
-		
-		
-		
+	public AirField() {
 	}
-	
+
+	public void readFleet() {
+
+		try (BufferedReader bufIn = new BufferedReader(new FileReader("jets.txt"))) {
+			String line;
+			List<Jet> jetList = new ArrayList<>();
+			while ((line = bufIn.readLine()) != null) {
+				String[] row = line.split(",");
+				String type = row[0];
+				String model = row[1];
+				Double speed = Double.parseDouble(row[2]);
+				Integer range = Integer.parseInt(row[3]);
+				Long price = Long.parseLong(row[4]);
+				if (type.equals("MilitaryCargo")) {
+					Jet temp = new MilitaryCargo(type, model, speed, range, price);
+					jetList.add(temp);
+
+				} else if (type.equals("Fighter")) {
+					jetList.add(new Fighter(type, model, speed, range, price));
+				} else if (type.equals("Passenger")) {
+					jetList.add(new Passenger(type, model, speed, range, price));
+				}
+
+			}
+			jets = jetList;
+
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+
+	}
+
 	public void listFleet() {
 		for (Jet jet : jets) {
 			System.out.println(jet);
+
+		}
+
+	}
+	
+	public String fastestJet(List<Jet> fastest, double speed) {
+		for (Jet jet : fastest) {
 			
 		}
 		
-	}
+
+	return "The fastest jet in the Fleet is: " + fastest +", at " + speed + " MPH.";
+}
 
 }
